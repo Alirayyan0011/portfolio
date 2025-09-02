2d8e290d7065ba1c7b9633ff84604980e468d3f6
alirayyan0011 <ali.rayyan001@gmail.com>
2025-09-02 10:59:40 +0500

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/Global/Navbar';
+import Sidebar from '@/components/Sidebar/Sidebar';
 import Hero from '@/components/HomePage/Hero';
 import CounterSection from '@/components/HomePage/Counter';
 import Footer from '@/components/Global/Footer';
@@ -6,16 +6,20 @@ import Spacer from '@/components/Global/Spacer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col">
-      <Navbar />
-      <div className="flex-grow">
-        <Hero />
-        <Spacer height={2} />
-        <CounterSection />
-        <Spacer height={2} />
-        {/* Additional sections would go here */}
+    <main className="min-h-screen flex">
+      <Sidebar />
+      
+      {/* Main content area */}
+      <div className="flex-1 flex flex-col lg:ml-0">
+        <div className="flex-grow">
+          <Hero />
+          <Spacer height={2} />
+          <CounterSection />
+          <Spacer height={2} />
+          {/* Additional sections would go here */}
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </main>
   );
 }
